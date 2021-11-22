efcaaa0bd9dde6fbfe2bf8e07ff6f59371910d96
Henry-Azer <henaryazer@outlook.com>
2021-11-22 22:05:56 +0200

    Finish AboutUs Route & Contact Section Responsive

--- src/components/about-us.jsx
+++ src/components/about-us.jsx
@@ -4,7 +4,8 @@ import AOS from "aos";
 
 import { BsChevronRight } from "react-icons/bs";
 
-import HenryAzerImg from "..//resources/images/henry-azer.jpg";
+import HenryProfileImg from "..//resources/images/henry-profile.png";
+import HenryBgImg from "..//resources/images/henry-background.jpg";
 import HenryJewelryImg from "..//resources/projects/henry-jewelry.png";
 import HenryHotelImg from "..//resources/projects/henry-hotel.png";
 import HenryTodoListImg from "..//resources/projects/henry-todo-list.png";
@@ -26,11 +27,15 @@ function AboutUs() {
             <Header />
 
             <div className="about-us-background"></div>
-            <hr />
+            <hr
+                data-aos-delay="100"
+                data-aos-duration="1500"
+                data-aos="fade-up"
+            />
 
             <div className="personal-info">
                 <div
-                    className="left-description"
+                    className="left-container"
                     data-aos-delay="100"
                     data-aos-duration="3000"
                     data-aos="fade-up-right"
@@ -52,54 +57,8 @@ function AboutUs() {
                     </p>
                 </div>
 
-                <div className="personal-card">
-                    <figure
-                        className="info-wrapper"
-                        data-aos-delay="100"
-                        data-aos-duration="3000"
-                        data-aos="fade-up"
-                    >
-                        <img src={HenryAzerImg} alt="henry-azer" />
-                        <figcaption>
-                            <h2>
-                                Henry &nbsp;<span>Azer</span>
-                            </h2>
-                            <span className="position">software engineer</span>
-                            <hr />
-                            <p>
-                                Skilled Full Stack Java/React Developer, Three+
-                                years of self-studying. Quick learner, working
-                                hard as self-teaching and looking forward to
-                                keeping pace with the times.
-                            </p>
-                        </figcaption>
-                    </figure>
-
-                    <div
-                        className="mid-description"
-                        data-aos-delay="50"
-                        data-aos-duration="1000"
-                        data-aos="fade-up"
-                    >
-                        <div className="title-heading-wrapper">
-                            <h1>Full-Stack</h1>
-                            <h3>Back-End & Front-End</h3>
-                        </div>
-                        <img
-                            src="https://chennaitraining.in/wp-content/uploads/2020/02/fullstack.png"
-                            alt="full-stack"
-                        />
-                        <p>
-                            Full-stack refers to a group of programming
-                            languages and tools a developer learns to handle
-                            both the front-end and back-end development of a
-                            website.
-                        </p>
-                    </div>
-                </div>
-
                 <div
-                    className="right-description"
+                    className="right-container"
                     data-aos-delay="100"
                     data-aos-duration="3000"
                     data-aos="fade-up-left"
@@ -120,15 +79,70 @@ function AboutUs() {
                         development of single-page and mobile applications.
                     </p>
                 </div>
+
+                <div className="mid-container">
+                    <figure
+                        className="personal-card"
+                        data-aos-delay="100"
+                        data-aos-duration="3000"
+                        data-aos="flip-down"
+                    >
+                        <img src={HenryBgImg} alt="henry-background" />
+                        <figcaption>
+                            <img
+                                src={HenryProfileImg}
+                                alt="henry-profile"
+                                className="profile"
+                            />
+                            <h2>
+                                Henry Azer<span>Software Engineer</span>
+                            </h2>
+                            <p>
+                                Skilled Full Stack Java/React Developer, Three+
+                                years of self-studying. Quick learner, working
+                                hard as self-teaching and looking forward to
+                                keeping pace with the times.
+                            </p>
+                        </figcaption>
+                    </figure>
+
+                    <div
+                        className="mid-wrapper"
+                        data-aos-delay="50"
+                        data-aos-duration="1000"
+                        data-aos="fade-up"
+                    >
+                        <div className="title-heading-wrapper">
+                            <h1>Full-Stack</h1>
+                            <h3>Back-End & Front-End</h3>
+                        </div>
+                        <img
+                            src="https://chennaitraining.in/wp-content/uploads/2020/02/fullstack.png"
+                            alt="full-stack"
+                        />
+                        <p>
+                            Full-stack refers to a group of programming
+                            languages and tools a developer learns to handle
+                            both the front-end and back-end development of a
+                            website.
+                        </p>
+                    </div>
+                </div>
             </div>
 
-            <div className="recent-projects">
-                <div
-                    className="title-heading-wrapper"
-                    data-aos-delay="100"
-                    data-aos-duration="1000"
-                    data-aos="fade-up"
-                >
+            <hr
+                data-aos-duration="2000"
+                data-aos-delay="100"
+                data-aos="fade-up"
+            />
+
+            <div
+                className="recent-projects"
+                data-aos-duration="2000"
+                data-aos-delay="100"
+                data-aos="fade-up"
+            >
+                <div className="title-heading-wrapper">
                     <h1>Recent Projects</h1>
                     <h3>Go through my recently completed projects</h3>
                 </div>
@@ -172,11 +186,16 @@ function AboutUs() {
                                 >
                                     <span>
                                         View Project
-                                        <BsChevronRight className="btn-arrow" />
+                                        <BsChevronRight className="btn-icon" />
                                     </span>
                                 </a>
                             </div>
-                            <figure className="project-img">
+                            <figure
+                                className="project-img"
+                                data-aos-delay="50"
+                                data-aos-duration="1500"
+                                data-aos="fade-in"
+                            >
                                 <img
                                     src={HenryJewelryImg}
                                     alt="henry-jewelry"
@@ -229,11 +248,16 @@ function AboutUs() {
                                 >
                                     <span>
                                         View Project
-                                        <BsChevronRight className="btn-arrow" />
+                                        <BsChevronRight className="btn-icon" />
                                     </span>
                                 </a>
                             </div>
-                            <figure className="project-img">
+                            <figure
+                                className="project-img"
+                                data-aos-delay="100"
+                                data-aos-duration="1500"
+                                data-aos="fade-in"
+                            >
                                 <img
                                     src={HenryHotelImg}
                                     alt="henry-hotel-reservation"
@@ -279,11 +303,16 @@ function AboutUs() {
                                 >
                                     <span>
                                         View Project
-                                        <BsChevronRight className="btn-arrow" />
+                                        <BsChevronRight className="btn-icon" />
                                     </span>
                                 </a>
                             </div>
-                            <figure className="project-img">
+                            <figure
+                                className="project-img"
+                                data-aos-delay="150"
+                                data-aos-duration="1500"
+                                data-aos="fade-in"
+                            >
                                 <img
                                     src={HenryTodoListImg}
                                     alt="henry-todo-list"
